310830097cb3e2b400b42be1015b8b44643f1029
svivian1993 <forestelf93@gmail.com>
2021-02-04 00:02:14 +0200

    Added checkbox in list item

--- app/src/main/java/com/example/composingachopinliszt/MainActivity.kt
+++ app/src/main/java/com/example/composingachopinliszt/MainActivity.kt
@@ -1,32 +1,23 @@
 package com.example.composingachopinliszt
 
 import android.os.Bundle
-import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.animateAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Divider
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -74,6 +65,7 @@ fun ChopinLiszt(names: List<String>) {
             ) {
                 ImageItem()
                 TextItem(name = name)
+                CheckBoxItem()
             }
             Divider(color = colorResource(R.color.teal_200))
         }
@@ -86,13 +78,25 @@ fun TextItem(name: String) {
         text = name,
         modifier = Modifier
             .padding(24.dp)
+            .fillMaxWidth(0.8f)
     )
 }
 
 @Composable
 fun ImageItem() {
     Image(
-        vectorResource(id = R.drawable.ic_android_black_24dp)
+        vectorResource(id = R.drawable.ic_android_black_24dp),
+                modifier = Modifier.fillMaxWidth(0.1f)
+    )
+}
+
+@Composable
+fun CheckBoxItem() {
+    val checkedState = remember { mutableStateOf(true) }
+    Checkbox(
+        modifier = Modifier.fillMaxWidth(0.1f),
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
     )
 }
 
